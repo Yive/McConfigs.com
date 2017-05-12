baec29a2622b0ada839ea8fcb7cd5b0094512800
Yive <admin@yive.me>
2017-05-12 15:50:42 -0700

    Update analytics

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -17,7 +17,7 @@ $('#essentials-locale').click(function () {$.bootstrapGrowl('English: "en"<br/>B
          while (count-- > 0) first_row.clone().appendTo('#kits-table');
      });
 
-     
+
      var table = $('#kits-table'),
          iter = 1;
      var table1 = $('#list-table'),
@@ -26,7 +26,7 @@ $('#essentials-locale').click(function () {$.bootstrapGrowl('English: "en"<br/>B
          table.find('tr').each(function(){
            var trow = $(this);
                  trow.append('<td><label>Kit Item #'+iter+'</label><input type="text" class="form-control" name="kits-items['+iter+'][]"  size="15"></td>');
-            
+
          });
          iter += 1;
      });
@@ -34,19 +34,17 @@ $('#essentials-locale').click(function () {$.bootstrapGrowl('English: "en"<br/>B
          table1.find('tr').each(function(){
            var trow = $(this);
                  trow.append('<td><label>Group #'+iter1+'</label><input type="text" class="form-control" name="list-groups['+iter1+'][]"  size="15"></td>');
-            
+
          });
          iter1 += 1;
      });
  });
-</script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-52657458-1', 'auto');
-  ga('send', 'pageview');
+   ga('create', 'UA-52657458-5', 'auto');
+   ga('send', 'pageview');
 
-</script>
+ </script>
